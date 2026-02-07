c93b070f65201093e80dd0ea72caa0bcfc6b927a
Liri Sokol <liri.sokol@cs.huji.ac.il>
2026-02-07 11:59:20 +0200

    outputs

---
+++ sbatch/gdrive_copy_to_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=1000
+#SBATCH --time=7-00:00:00
+#SBATCH -e sync_logs/rclone.err
+#SBATCH -o sync_logs/rclone.out
+module load hurcs rclone
+
+cd /sci/labs/dh_huji/liri.sokol/journals-keyword-searching
+
+source_dir=G-l:
+dest_dir=pdfs
+log_path=sync_logs/rclone.log
+rclone copy \
+  --drive-root-folder-id=1iqqwrcwqRxoKyxNuZeVsVMubLMgt864H \
+  --fast-list \
+  --cache-rps 50 -v --tpslimit 20 --tpslimit-burst 20 \
+  --checkers 32 --transfers 16 \
+  $source_dir $dest_dir 2>&1 | tee $log_path
+
