d0a518b428281a7683f9f62d28f9e98954ed1f7f
ReallyLiri <reallyliri@gmail.com>
2026-05-21 20:04:40 +0300

    log name

--- sbatch/authors_works.sh
+++ sbatch/authors_works.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=2000
 #SBATCH --time=7-00:00:00
-#SBATCH -e sync_logs/rclone.err
-#SBATCH -o sync_logs/rclone.out
+#SBATCH -e sync_logs/authors_works.err
+#SBATCH -o sync_logs/authors_works.out
 
 PYTHONPATH="${SRC_DIR}" uv run authors_works.py
