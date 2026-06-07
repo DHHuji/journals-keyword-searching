ace828e3402899c107c276e2654ae6f45066bc66
Liri Sokol <liri.sokol@cs.huji.ac.il>
2026-06-07 10:36:47 +0300

    authors

--- sbatch/authors_works_agg.sh
+++ sbatch/authors_works_agg.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4000
 #SBATCH --time=7-00:00:00
 #SBATCH -e sync_logs/authors_works.err
 #SBATCH -o sync_logs/authors_works.out
 
-PYTHONPATH="${SRC_DIR}" uv run authors_works.py
-
+PYTHONPATH="${SRC_DIR}" uv run authors_works_aggregate.py
